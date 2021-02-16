8c78017a56489146c4ffbb819fa51ba1fe4a80cc
LeeJinWoo <dlwlsdn201@naver.com>
2021-02-16 16:26:19 +0900

    {add}: 일일 박스오피스 영화 데이터 로드 스크립트 구현
    <description>
        - axios 기능과 영화진흥위원회에서 제공하는 오픈 API 데이터를 이용하여 날짜별 박스오피스 1~10위 영화에 대한 json 데이터를 요청/로드 하는 스크립트 추가
    
    <issue-number>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [date, setDate] = useState(null);
+
+  const getMovies = async () => {
+    const {
+      data: {
+        boxOfficeResult: {dailyBoxOfficeList}
+      }
+    } = await axios.get(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=4010de0e4173634fe5b671b20aea7c21&targetDt=${date}`);
+    setMovies({dailyBoxOfficeList});
+    console.log(dailyBoxOfficeList);
+    setLoading(false);
+  };
+  
+  useEffect(()=> {
+    if(date)
+      getMovies();
+  }, [date]);
+
+  const dateHandler = (e) => {
+      let inputDate ='';
+      inputDate += e.target.value;
+      if(inputDate.length === 8)
+        setDate(e.target.value);
+  };
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <input 
+        type="text" 
+        placeholder="날짜 입력 예) 20210214"
+        onChange={dateHandler}/>
+      <div>{loading? "Loading data.." : "Success load"}
+      </div>
+    </>
   );
 };
 
