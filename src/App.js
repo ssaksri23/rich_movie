8c5c45d94baa445298af3a535ade0464786e1183
LeeJinWoo <dlwlsdn201@naver.com>
2021-02-18 15:41:11 +0900

    국외/국내별 영화 데이터 로드 기능의 버튼 구현_ closed #2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React,{useState, useEffect} from 'react';
 import axios from 'axios';
+import Movie from './components/Movie';
+
 
 const App = () => {
 
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
   const [date, setDate] = useState(null);
-
+  const [nation, setNation] = useState('K')
   const getMovies = async () => {
     const {
       data: {
         boxOfficeResult: {dailyBoxOfficeList}
       }
-    } = await axios.get(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=4010de0e4173634fe5b671b20aea7c21&targetDt=${date}`);
+    } = await axios.get(`http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=4010de0e4173634fe5b671b20aea7c21&targetDt=${date}&repNationCd=${nation}`);
     setMovies({dailyBoxOfficeList});
     console.log(dailyBoxOfficeList);
     setLoading(false);
@@ -30,11 +32,20 @@ const App = () => {
         setDate(e.target.value);
   };
 
+  const NationHandler = (e) => {
+    let Nation = e.target.value === 'K'? setNation('K') : setNation('F');
+    return Nation;
+  }
+
   return (
     <>
+      <div className="buttonBlock">
+        <button value="K" onClick={NationHandler}>국내영화</button>
+        <button value="F" onClick={NationHandler}>해외영화</button>
+      </div>
       <input 
         type="text" 
-        placeholder="날짜 입력 예) 20210214"
+        placeholder="조회 날짜 입력 예) 20210214"
         onChange={dateHandler}/>
       <div>{loading? "Loading data.." : "Success load"}
       </div>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,18 @@
+//노마드코더 rendering Movies 영상
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Movie = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    return <h1>Movie Components</h1>
 };
+
+Movie.propTypes = {
+    id: PropTypes.string.isRequired,  // id = movieCd
+    title : PropTypes.string.isRequired, //title = movieNm
+    openDt : PropTypes.string.isRequired,
+    rank: PropTypes.string.isRequired,
+    rankOldAndNew: PropTypes.string.isRequired,
+    audiAcc: PropTypes.string.isRequired,
+}
 
 export default Movie;
